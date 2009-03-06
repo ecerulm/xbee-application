32579c6cb4f9b9146713023e452863936eb8c058
ruben.laguna <ruben.laguna@b58498fe-0643-11de-9c5c-7db7966a26d7>
2009-03-06 06:40:21 +0000

    Added license info

--- XBeeApplication/XBeeModule/src/com/rubenlaguna/xbeemodule/HexUtils.java
+++ XBeeApplication/XBeeModule/src/com/rubenlaguna/xbeemodule/HexUtils.java
@@ -1,8 +1,22 @@
 /*
- * Created on 11-abr-2004
+ * HexUtils.java
  *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ * Copyright (c) 2009 Ruben Laguna <ruben.laguna at gmail.com>. All rights reserved.
+ *
+ * This file is part of XBeeApplication.
+ *
+ * XBeeApplication is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XBeeApplication is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XBeeApplication.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jletty.util;
 
